ddc8a7790e1346c279de604e5a1472fb55b9b9b9
tbragga <autorizacaobraga@gmail.com>
2026-04-17 21:56:27 -0300

    Delete api/webhook.js

--- api/webhook.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (req, res) => {
-  if (req.method !== 'POST') return res.status(405).end();
-  const { type, data } = req.body;
-  if (type === 'payment') {
-    console.log('Pagamento aprovado:', data.id);
-    // Aqui você pode depois atualizar o Supabase
-  }
-  res.status(200).end();
-};
